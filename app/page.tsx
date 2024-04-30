54d1e6532bc9c03032b72c9e8d92a251392780ea
guijun13 <guilhermejun13.grillo@gmail.com>
2024-04-30 15:43:28 -0300

    feat: fetch and display orders data

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,46 @@ import OrdersTable from '@/components/orders-table';
 import Pagination from '@/components/pagination';
 import SearchInput from '@/components/search-input';
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+
+export interface orderData {
+  id: number;
+  customer_name: string;
+  customer_email: string;
+  order_date: Date;
+  amount_in_cents: number;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+async function getOrdersData() {
+  const response = await fetch('https://apis.codante.io/api/orders-api/orders');
+  const orders = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return orders.data;
+}
 
 export default async function Component() {
+  const ordersData: orderData[] = await getOrdersData();
+
   return (
     <main className="container px-1 py-10 md:p-10">
       <Card>
         <CardHeader className="px-7">
           <CardTitle>Pedidos</CardTitle>
-          <CardDescription>
-            Uma listagem de pedidos do seu negócio.
-          </CardDescription>
+          <CardDescription>Uma listagem de pedidos do seu negócio.</CardDescription>
           <div className="flex pt-10 gap-4">
             <SearchInput />
             <FilterDropdown />
           </div>
         </CardHeader>
         <CardContent>
-          <OrdersTable />
+          <OrdersTable ordersData={ordersData} />
           <div className="mt-8">
             <Pagination />
           </div>
--- components/orders-table.tsx
+++ components/orders-table.tsx
@@ -8,8 +8,14 @@ import {
 } from '@/components/ui/table';
 import { Badge } from './ui/badge';
 import { ChevronsUpDown } from 'lucide-react';
+import { orderData } from '@/app/page';
 
-export default function OrdersTable() {
+function centsToReal(value: number): string {
+  const realValue = value / 100;
+  return `${realValue.toFixed(2).replace('.', ',')}`;
+}
+
+export default function OrdersTable({ ordersData }: { ordersData: orderData[] }) {
   return (
     <Table>
       <TableHeader>
@@ -29,36 +35,23 @@ export default function OrdersTable() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Fulano de Tal</div>
-            <div className="hidden md:inline text-sm text-muted-foreground">
-              fulano.de.tal@gmail.com
-            </div>
-          </TableCell>
-          <TableCell>
-            <Badge className={`text-xs`} variant="outline">
-              Pendente
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2024-01-01</TableCell>
-          <TableCell className="text-right">R$100,00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Ciclana de Tal</div>
-            <div className="text-sm text-muted-foreground">
-              ciclana.de.tal@gmail.com
-            </div>
-          </TableCell>
-          <TableCell>
-            <Badge className={`text-xs`} variant="outline">
-              Completo
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-01-01</TableCell>
-          <TableCell className="text-right">R$500,00</TableCell>
-        </TableRow>
+        {ordersData.map((order: orderData) => (
+          <TableRow key={order.id}>
+            <TableCell>
+              <div className="font-medium">{order.customer_name}</div>
+              <div className="hidden md:inline text-sm text-muted-foreground">
+                {order.customer_email}
+              </div>
+            </TableCell>
+            <TableCell>
+              <Badge className={`text-xs`} variant="outline">
+                {order.status}
+              </Badge>
+            </TableCell>
+            <TableCell className="hidden md:table-cell">{order.order_date.toString()}</TableCell>
+            <TableCell className="text-right">R${centsToReal(order.amount_in_cents)}</TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
